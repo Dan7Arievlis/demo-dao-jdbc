f612278f9bf16496c10f54132965ff6783820d29
Daniel Neves <73949890+Dan7Arievlis@users.noreply.github.com>
2025-06-23 23:37:05 -0300

    DAO interfaces added

---
+++ src/main/java/com/danielsilveira/model/dao/Dao.java
@@ -0,0 +1,15 @@
+package com.danielsilveira.model.dao;
+
+import java.util.List;
+
+public interface Dao<T> {
+    void insert(T obj);
+
+    void update(T obj);
+    
+    void deleteById(Integer id);
+    
+    T findById(Integer id);
+    
+    List<T> findAll();
+}
