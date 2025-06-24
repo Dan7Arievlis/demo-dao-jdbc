94c1caaa29955b690201d018179fdfa8475f1b34
Daniel Neves <73949890+Dan7Arievlis@users.noreply.github.com>
2025-06-24 14:09:27 -0300

    Insert implementation

--- src/test/java/com/danielsilveira/model/dao/impl/SellerDaoJDBCTest.java
+++ src/test/java/com/danielsilveira/model/dao/impl/SellerDaoJDBCTest.java
@@ -1,44 +1,51 @@
 package com.danielsilveira.model.dao.impl;
 
+import java.util.Collections;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import static org.mockito.ArgumentMatchers.any;
+import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 
-import com.danielsilveira.model.dao.DaoFactory;
-import com.danielsilveira.model.dao.SellerDao;
 import com.danielsilveira.model.entities.Department;
 import com.danielsilveira.model.entities.Seller;
 
 @ExtendWith(MockitoExtension.class)
 public class SellerDaoJDBCTest {
-    Seller seller;
-    SellerDao sellerDao;
+    @Mock
+    SellerDaoJDBC sellerDao;
+    @Mock
+    Department department;
 
     @Test
     public void shouldReturnSeller() {
-        // Mockito.when(sellerDao.findById(3)).thenReturn(new Seller(3, "Daniel", "daniel@dev.com", new Date(), 3000.0, department));
-        sellerDao = DaoFactory.createSellerDao();
-        seller = sellerDao.findById(3);
-
+        Mockito.when(sellerDao.findById(3)).thenReturn(new Seller());
+        Seller seller = sellerDao.findById(3);
         assertTrue(seller instanceof Seller);
     }
 
     @Test
     public void testFindByDepartment() {
-        sellerDao = DaoFactory.createSellerDao();
-        Department dep = new Department(2, null);
-        List<Seller> sellers = sellerDao.findByDepartment(dep);
-        assertEquals(2, sellers.size());
+        Mockito.when(sellerDao.findByDepartment(department)).thenReturn(Collections.singletonList(any(Seller.class)));
+        List<Seller> sellers = sellerDao.findByDepartment(department);
+        assertEquals(1, sellers.size());
     }
-
+    
     @Test
     public void shouldReturnAllTheSellerTable() {
-        sellerDao = DaoFactory.createSellerDao();
+        Mockito.when(sellerDao.findAll()).thenReturn(Collections.nCopies(5, new Seller()));
         List<Seller> sellers = sellerDao.findAll();
-        assertTrue(!sellers.isEmpty());
+        assertEquals(5, sellers.size());
+    }
+    @Test
+    void testInsert() {
+        Mockito.doNothing().when(sellerDao).insert(any(Seller.class));
+        sellerDao.insert(new Seller());
+        Mockito.verify(sellerDao).insert(any(Seller.class));
     }
 }
